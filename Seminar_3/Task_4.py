a6a0507b6803828f06a493ea578c36243430df25
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-18 00:06:32 +0300

    Task_4 complete

--- Seminar_3/Task_4.py
+++ Seminar_3/Task_4.py
@@ -5,3 +5,18 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+num = int(input('Введите число: '))
+
+
+def get_binary(number):
+    number_binary = ' '
+    while number > 0:
+        number_binary += str(number % 2)
+        number //= 2
+        rev_num_bin=''.join(reversed(number_binary))
+    return rev_num_bin
+
+
+num_bin = get_binary(num)
+print(num_bin)
