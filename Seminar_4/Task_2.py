c2bf8e6f36937ac995614a556e222affad72e46c
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-22 16:25:32 +0300

    Task_2 complete

--- Seminar_4/Task_2.py
+++ Seminar_4/Task_2.py
@@ -1,4 +1,27 @@
 
-# 2- Задайте последовательность чисел. Напишите программу, которая 
-# выведет список неповторяющихся элементов исходной последовательности. 
+# 2- Задайте последовательность чисел. Напишите программу, которая
+# выведет список неповторяющихся элементов исходной последовательности.
 # Посмотрели, что такое множество? Вот здесь его не используйте.
+
+import random
+
+
+num = [random.randint(0, 25)for i in range(0, 101)]
+
+
+def get_unique_numbers(numbers):
+    unique = []
+    for number in numbers:
+        if number in unique:
+            continue
+        else:
+            unique.append(number)
+    return unique
+
+
+uni_num = get_unique_numbers(num)
+new_num=list(set(num))
+
+print(num)
+print(uni_num)
+print(new_num)
