de6be076fab2876c7560d0d8f4effdbb17d97b20
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-22 14:41:07 +0300

    Task_3 complete

--- Seminar_4/Task_3.py
+++ Seminar_4/Task_3.py
@@ -1,2 +1,18 @@
 # 3- Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # Пример: при N = 12 -> [2, 3]
+
+
+n = int(input('Введите число: '))
+div = 2
+res_lst = []
+
+while n > 1:
+    while n % div == 0:
+        res_lst.append(div)
+        n = n/div
+    # div += 1
+    if div == 2:
+        div += 1
+    else:
+        div += 2
+print(res_lst)
