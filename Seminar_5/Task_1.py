7fc273bc0c94c0cf59175c6972c784e746296c25
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-23 21:12:14 +0300

    Task_1 complete

--- Seminar_5/Task_1.py
+++ Seminar_5/Task_1.py
@@ -1,3 +1,21 @@
 # 1- Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 # 'абвгдейка - это передача' = >" - это передача"
 
+input_text = 'абв абвгдейка, абвгдейка - это и учёба, и игра.'
+
+print(input_text)
+
+
+def delete_words(text):
+    words = text.split()
+    fragment = 'абв'
+    new_words = []
+    for word in words:
+        if fragment not in word:
+            new_words.append(word)
+    new_words = ' '.join(new_words)
+    return new_words
+
+
+result_text = delete_words(input_text)
+print(result_text)
