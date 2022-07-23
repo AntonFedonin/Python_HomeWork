92b3bb653c357d57c6b8e424ba07d5483b19ec66
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-23 18:41:22 +0300

    Modified Task_4

--- Seminar_4/Task_4.py
+++ Seminar_4/Task_4.py
@@ -5,10 +5,11 @@
 # https://zen.yandex.ru/suite/a6424a0f-4fdb-44a1-95a0-9945e6f0a699
 # Это на случай возникновения непонятных символов в файле.
 
+
 import re
 
-new_file = open('Text.txt', 'w')
 input_str = input('Напишите редактируемый текст: ')
+new_file = open('Text.txt', 'w')
 new_file.writelines(input_str)
 new_file.close()
 print(input_str)
@@ -18,8 +19,11 @@ with open('Text.txt', 'r') as new_file:
         output_str = line
 print(output_str)
 
-output_str = re.sub("[0|1|2|3|4|5|6|7|8|9]", "", output_str)
+
+output_str = re.sub("[^А-Яа-я- ]", "", input_str)
+
 
 with open('Text.txt', 'a') as new_file:
     new_file.write('\n' + output_str)
 print(output_str)
+
