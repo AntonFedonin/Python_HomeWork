44222250d209a73dc7c4e7fcf229585a0690228c
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-23 19:23:17 +0300

    Modified Task_3 with function

--- Seminar_4/Task_3.py
+++ Seminar_4/Task_3.py
@@ -1,18 +1,30 @@
 # 3- Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # Пример: при N = 12 -> [2, 3]
 
+def digit_check():
+    try:
+        lenght = int(input('Введите число:\n'))
+        return lenght
+    except ValueError:
+        print('Должно быть число!:\n')
+        digit_check()
 
-n = int(input('Введите число: '))
-div = 2
-res_lst = []
 
-while n > 1:
-    while n % div == 0:
-        res_lst.append(div)
-        n = n/div
-    # div += 1
-    if div == 2:
-        div += 1
-    else:
-        div += 2
+def get_multipliers(number):
+    div = 2
+    result_lst = []
+    while number > 1:
+        while number % div == 0:
+            result_lst.append(div)
+            number = number/div
+        # div += 1
+        if div == 2:
+            div += 1
+        else:
+            div += 2
+    return result_lst
+
+
+n = digit_check()
+res_lst = get_multipliers(n)
 print(res_lst)
